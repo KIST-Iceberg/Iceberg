e6cfc2a51ce364c788460fe946ae7282225a2003
karios03 <kairos9603@gmail.com>
2017-12-15 12:06:02 +0100

    fix wrong shuffle

--- data_input.py
+++ data_input.py
@@ -1,38 +1,19 @@
 # Copy Right Kairos03 2017. All Right Reserved.
 
-import pandas as pd
 import numpy as np
-
-# load the data set
-train = pd.read_json("data/processed/train.json")
+import pickle
 
 
-# test = pd.read_json("data/processed/test.json")
-
-
-# generate the training data
-# create 3 bands having HH, HV and avg of both
-def gen_new_data(data_list):
-    X_band_1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in data_list["band_1"]])
-    X_band_2 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in data_list["band_2"]])
-    X = np.concatenate([X_band_1[:, :, :, np.newaxis], X_band_2[:, :, :, np.newaxis],
-                        ((X_band_1 + X_band_2) / 2)[:, :, :, np.newaxis]], axis=-1)
-    Y = np.eye(2)[np.asarray(train['is_iceberg'])]
-
-    return X, Y
-
-
-# train
-x_train, x_label = gen_new_data(train)
-
-
-# d_test = gen_new_data(test)
+origin_path = 'data/processed/train.json'
+pp_x_path = 'data/processed/pp_train_x.pkl'
+pp_label_path = 'data/processed/pp_train_label.pkl'
+test_path = "data/processed/test.json"
 
 
 class Dataset:
-    def __init__(self, batch_size):
-        self.train_data = x_train
-        self.train_label = x_label
+    def __init__(self, batch_size, x, y):
+        self.train_data = x
+        self.train_label = y
         # self.test = x_label
         self.train_size = self.train_data.shape[0]
         # self.test_size = self.test.shape[0]
@@ -42,7 +23,8 @@ class Dataset:
 
     def next_batch(self):
         if self.batch_cnt == 0:
-            np.random.shuffle(self.train_data)
+            pass
+            # np.random.shuffle(self.train_data)
 
         start = self.batch_cnt * self.batch_size
         self.batch_cnt += 1
@@ -59,13 +41,23 @@ class Dataset:
     #     return self.test[:][0], self.test[:][1]
 
 
-def get_dataset(batch_size):
-    return Dataset(batch_size=batch_size)
+def get_dataset(batch_size, x, y):
+    return Dataset(batch_size=batch_size, x=x, y=y)
+
+
+def load_data():
+    return pickle.load(open(pp_x_path, 'rb')), pickle.load(open(pp_label_path, 'rb'))
 
 
 if __name__ == '__main__':
-    d = get_dataset(100)
+
+    x, y = load_data()
+
+    dd = get_dataset(100, x, y)
+
     for e in range(5):
-        for b in range(16):
-            xs, ys = d.next_batch()
+        for b in range(dd.total_batch):
+            xs, ys = dd.next_batch()
             print(e, b, xs.shape, ys.shape)
+
+
