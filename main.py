9fbf2b62bcf1186ebadd2bb2df60db3a7560ac51
karios03 <kairos9603@gmail.com>
2018-01-08 15:47:05 +0100

    Revert "Revert "Exp 5 current""
    This reverts commit 95fd719facb3a40598fe69257df47bd6a341b3e1.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # Copy Right Kairos03 2017. All Right Reserved.
+"""
+main
+"""
 
 import train
 import test
 
 
 if __name__ == '__main__':
-    train.train(is_valid=False)
-    test.test(train.LOG_TRAIN_PATH, is_test=True)
+    train.train(is_valid=True)
+    # test.test(train.LOG_TRAIN_PATH, is_test=True)
