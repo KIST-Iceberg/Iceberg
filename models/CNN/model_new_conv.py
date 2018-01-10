dd4c505072c9d1c4edb3a6c9485431a2b9cd774f
karios03 <kairos9603@gmail.com>
2018-01-10 11:49:39 +0100

    Current BEST logloss 0.06105

--- models/CNN/model_new_conv.py
+++ models/CNN/model_new_conv.py
@@ -14,27 +14,32 @@ def var_summary(var):
         tf.summary.scalar('mean', mean)
         stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
         tf.summary.scalar('stddev', stddev)
-
+        
 
 def make_model(X, Y, A, keep_prob, learning_rate):
+    with tf.name_scope('input_images'):
+        tf.summary.image('origin', X[:, :, :, 0:3], 1)
+        tf.summary.image('lee', X[:, :, :, 3:6], 1)
+        tf.summary.image('high', X[:, :, :, 6:9], 1)
 
-    tf.summary.image('origin', X[:,:,:,0:3])
-    tf.summary.image('lee', X[:,:,:,3:6])
-    tf.summary.image('high', X[:,:,:,6:9])
+    with tf.variable_scope('hyperparam'):
+        tf.summary.scalar('learning_rate', learning_rate)
 
     with tf.variable_scope('conv1'):
         bn = tf.layers.batch_normalization(X)
-        conv = tf.layers.conv2d(bn, 64, (3, 3),strides=(2, 2), activation=tf.nn.relu)
-        drop = tf.layers.dropout(conv)
+        conv = tf.layers.conv2d(
+            bn, 64, (3, 3), strides=(2, 2), activation=tf.nn.relu)
+        drop = tf.layers.dropout(conv, keep_prob)
         print('[{:s}] \t | {}'.format('conv1', drop.shape))
-        
+
         for var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='conv1'):
             var_summary(var)
 
     with tf.variable_scope('conv2'):
         bn = tf.layers.batch_normalization(drop)
-        conv = tf.layers.conv2d(bn, 128, (3, 3),strides=(2, 2), activation=tf.nn.relu)
-        drop = tf.layers.dropout(conv)
+        conv = tf.layers.conv2d(
+            bn, 128, (3, 3), strides=(2, 2), activation=tf.nn.relu)
+        drop = tf.layers.dropout(conv, keep_prob)
         print('[{:s}] \t | {}'.format('conv2', drop.shape))
 
         for var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='conv2'):
@@ -42,22 +47,24 @@ def make_model(X, Y, A, keep_prob, learning_rate):
 
     with tf.variable_scope('conv3'):
         bn = tf.layers.batch_normalization(drop)
-        conv = tf.layers.conv2d(bn, 128, (3, 3),strides=(2, 2), activation=tf.nn.relu)
-        drop = tf.layers.dropout(conv)
+        conv = tf.layers.conv2d(
+            bn, 128, (3, 3), strides=(2, 2), activation=tf.nn.relu)
+        drop = tf.layers.dropout(conv, keep_prob)
         print('[{:s}] \t | {}'.format('conv3', drop.shape))
 
         for var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='conv3'):
             var_summary(var)
-    
+
     with tf.variable_scope('conv4'):
         bn = tf.layers.batch_normalization(drop)
-        conv = tf.layers.conv2d(bn, 64, (3, 3),strides=(2, 2), activation=tf.nn.relu)
-        drop = tf.layers.dropout(conv)
+        conv = tf.layers.conv2d(
+            bn, 64, (3, 3), strides=(2, 2), activation=tf.nn.relu)
+        drop = tf.layers.dropout(conv, keep_prob)
         print('[{:s}] \t | {}'.format('conv4', drop.shape))
 
         for var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='conv4'):
             var_summary(var)
-    
+
     with tf.variable_scope('reshape'):
         layer = tf.reshape(drop, (-1, 3 * 3 * 64))
         print('[{:s}] \t | {}'.format('reshape', layer.shape))
@@ -65,25 +72,25 @@ def make_model(X, Y, A, keep_prob, learning_rate):
     with tf.variable_scope('add_data'):
         layer = tf.concat((layer, A), axis=1)
         print('[{:s}] \t | {}'.format('add_data', layer.shape))
-    
+
     with tf.variable_scope('dense1'):
         bn = tf.layers.batch_normalization(layer)
         dense = tf.layers.dense(bn, 1024, activation=tf.nn.relu)
-        drop = tf.layers.dropout(dense)
+        drop = tf.layers.dropout(dense, keep_prob)
         print('[{:s}] \t | {}'.format('dense1', drop.shape))
 
         for var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='dense1'):
             var_summary(var)
-    
+
     with tf.variable_scope('dense2'):
         bn = tf.layers.batch_normalization(drop)
         dense = tf.layers.dense(bn, 512, activation=tf.nn.relu)
-        drop = tf.layers.dropout(dense)
+        drop = tf.layers.dropout(dense, keep_prob)
         print('[{:s}] \t | {}'.format('dense2', drop.shape))
 
         for var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='dense2'):
             var_summary(var)
-    
+
     output = tf.layers.dense(drop, 2, name='output')
     print('[{:s}] \t | {}'.format('output', output.shape))
 
@@ -101,5 +108,6 @@ def make_model(X, Y, A, keep_prob, learning_rate):
 
         tf.summary.scalar('accuracy', accuracy)
         proba = tf.nn.softmax(output, name='proba')
+    
 
     return output, xent, optimizer, accuracy
