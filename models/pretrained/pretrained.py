1dc18b62a764984a93a145e7537a7e74d329ebbf
karios03 <kairos9603@gmail.com>
2018-01-16 16:11:02 +0100

    final pretrain model

--- models/pretrained/pretrained.py
+++ models/pretrained/pretrained.py
@@ -18,28 +18,27 @@ from keras.layers import Input
 sys.path.append(os.path.abspath(os.path.join('.')))
 from data import process
 
-# from data import data_input
+TOTAL_EPOCH = 300
 
-input_tensor = Input(shape=(48, 48, 3))
-input_shape = (48, 48, 3)
+# from data import data_input
+input_tensor = Input(shape=(75, 75, 3))
 factory = {
-    'vgg16': lambda: VGG16(include_top=False, input_shape=input_shape, weights='imagenet'),
-    'vgg19': lambda: VGG19(include_top=False, input_shape=input_shape, weights='imagenet'),
-    'xception': lambda: Xception(include_top=False, input_tensor=input_tensor, weights='imagenet')
-    # 'InceptionV3': lambda: InceptionV3(include_top=False, input_tensor=input_tensor, weights='imagenet')
-    # 'InceptionResNetV2': lambda: InceptionResNetV2(include_top=False, input_tensor=input_tensor, weights='imagenet'),
+    # 'vgg16': lambda: VGG16(include_top=False, input_tensor=input_tensor, weights='imagenet'),
+    'vgg19': lambda: VGG19(include_top=False, input_tensor=input_tensor, weights='imagenet'),
+    'xception': lambda: Xception(include_top=False, input_tensor=input_tensor, weights='imagenet'),
+    'InceptionV3': lambda: InceptionV3(include_top=False, input_tensor=input_tensor, weights='imagenet'),
+    'InceptionResNetV2': lambda: InceptionResNetV2(include_top=False, input_tensor=input_tensor, weights='imagenet')
 }
 
 def get_model(name='simple',train_base=True,use_angle=False,dropout=0.8,layers=(512,256)):
-
 
     base = factory[name]()
     inputs = [base.input]
     x = GlobalMaxPooling2D()(base.output)
 
     if use_angle:
-        angle_in = Input(shape=(1,))
-        angle_x = Dense(1, activation='relu')(angle_in)
+        angle_in = Input(shape=(5,))
+        angle_x = Dense(5, activation='relu')(angle_in)
         inputs.append(angle_in)
         x = concatenate([x, angle_x])
 
@@ -54,65 +53,57 @@ def get_model(name='simple',train_base=True,use_angle=False,dropout=0.8,layers=(
 
     return Model(inputs=inputs, outputs=x)
 
-def crop(images):
+def train():
+    # train = pd.read_json('data/origin/train.json')
+    test  = pd.read_json("data/origin/test.json")
 
-    assert images.shape[1] == images.shape[2] == 75
-    assert images.shape[3] == 3
+    keys = list(factory.keys())
+    keys.sort()
 
-    proc = []
-    for idx in range(images.shape[0]):
-        img = images[idx]
-        img = np.reshape(img, (75, 75, 3))
-        im = img[:,:,0]
-        cx = im.argmax() // 75
-        cy = im.argmax() % 75
-        b1 = process.crop_center(img[:,:,0], cx, cy)
-        b2 = process.crop_center(img[:,:,1], cx, cy)
-        b3 = process.crop_center(img[:,:,2], cx, cy)
-        img = np.stack([b1,b2,b3], axis=2)
-        proc.append(img)
-    final = np.stack(proc)
-    print('crop', final.shape)
-    return final
-    
-
-def get_data(data, is_test=False):
-    """
-    """
-    b1 = np.array(data["band_1"].values.tolist()).reshape(-1, 75, 75, 1)
-    b2 = np.array(data["band_2"].values.tolist()).reshape(-1, 75, 75, 1)
-    b3 = abs(b1) + abs(b2)
-
-    X = crop(np.stack([b1,b2,b3], axis=3))
-    Y = np.array(data['is_iceberg']) if not is_test else None
-    angle = np.array(pd.to_numeric(data['inc_angle'], errors='coerce').fillna(0))
-
-    return X, Y, angle
-
-train = pd.read_json('data/origin/train.json')
-test  = pd.read_json("data/origin/test.json")
-
-keys = list(factory.keys())
-keys.sort()
-for model_name in keys:
-    print(model_name)
     # train data
-    X, Y, angle = get_data(train)
+    X, Y, angle = process.load_from_pickle()
+    T_X, T_Y, T_angle = process.load_from_pickle(is_test=True)
 
-    # model
-    model = get_model(model_name, train_base=False, use_angle=True)
-    model.compile(loss='binary_crossentropy', optimizer=Adam(lr=3e-4), metrics=['accuracy'])
+    for model_name in keys:
+        print(model_name)
+        
+        # model
+        model = get_model(model_name, train_base=False, use_angle=True)
+        model.compile(loss='binary_crossentropy', optimizer=Adam(lr=1e-4), metrics=['accuracy'])
 
-    # fit
-    history = model.fit([X, angle], Y, shuffle=True, verbose=1, epochs=1000)
+        # fit
+        history = model.fit([X, angle], Y, shuffle=True, verbose=1, epochs=TOTAL_EPOCH)
 
-    # submit
-    X, Y, angle = get_data(test, is_test=True)
-    predicted_test = model.predict([X, angle])
-    
-    print(predicted_test)
+        # submit
+        predicted_test = model.predict([T_X, T_angle])
+        
+        print(predicted_test)
 
-    submission = pd.DataFrame()
-    submission['id'] = test['id']
-    submission['is_iceberg'] = predicted_test.reshape((predicted_test.shape[0]))
-    submission.to_csv(model_name+'_sub.csv', index=False)
+        submission = pd.DataFrame()
+        submission['id'] = test['id']
+        submission['is_iceberg'] = predicted_test.reshape((predicted_test.shape[0]))
+        submission.to_csv("submissions/"+model_name+'_ep_'+str(TOTAL_EPOCH)+'.csv', index=False)
+
+
+def ensemble():
+    # ensemble
+    print('ENSEMBLE')
+    files = os.listdir('submissions')
+    subs = []
+    for file in files:
+        path = 'submissions/'+file
+        subs.append(pd.read_csv(path))
+        print('[READ] ', 'submissions/'+file)
+
+    final = pd.DataFrame()
+    final['id'] = subs[0]['id']
+    final['is_iceberg'] = np.exp(np.mean(
+        [sub['is_iceberg'].apply(lambda x: np.log(x)) for sub in subs], axis=0))
+    final.to_csv('submissions/ensamble.csv', index=False, float_format='%.6f')
+
+    del files
+
+
+if __name__ == '__main__':
+    # train()
+    ensemble()
